038213e658b405d3e5277c60fe993986f7032016
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-01-27 13:51:56 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ self.addEventListener('push', function(event) {
   const options = {
     body: data.message || data.body || 'Tens uma nova mensagem.',
     icon: '/icon-192.png',
-    badge: '',
+    badge: '/badge-96.png',
     vibrate: [200, 100, 200, 100, 200],
     data: {
       url: data.url || '/'
