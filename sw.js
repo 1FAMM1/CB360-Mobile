a56f7bd396a7407efbf2e25728e66fc899041a66
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2026-08-16 09:26:13 +0100

    Update cache name to version 5.2.4

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
     v5.2.2 - Fix: Instalação resiliente em cache (substituído
     cache.addAll por adição individual) e correção do fetch handler.
     ========================================================= */
-    const CACHE_NAME = 'cb360-cache-v5.2.3';
+    const CACHE_NAME = 'cb360-cache-v5.2.4';
     const ASSETS_TO_CACHE = ['/', '/index.html', '/MainPage.html', '/ScalesView.html', '/Swaps.html', '/MainPageEl.html', '/PiqDisp.html', '/DecDisp.html', '/SBADisp.html', '/OPATDisp.html',
                              '/ExtDisp.html', '/DispView.html', '/SolVacat.html', '/SolFardam.html', '/Attendance.html', '/OnGoingOcr.html', '/FomioPage.html', '/Events.html', '/MissReport.html',
                              '/Documents.html', '/Comunic.html', '/MeteoAdv.html', '/NoHospital.html', '/MainPageVe.html', '/VeicStat.html', '/VeicSitop.html', '/VeicData.html', '/VeicAnomalies.html',
